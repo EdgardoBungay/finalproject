dded6375f8c7977dd2d3d106b98966ec982a40fc
adams100 <tyleradams918@hotmail.com>
2020-01-13 15:18:20 -0800

    fixes to html

--- app.py
+++ app.py
@@ -12,7 +12,11 @@ def index():
         return render_template("index.html", result_table=result_table, model_score=model_score, params_table=params_table, conf_table=conf_table, class_report=class_report)  
     return render_template("index.html", result_table=result_table, model_score=model_score, params_table=params_table, conf_table=conf_table, class_report=class_report)
 
-
+@app.route("/logistic", methods=["GET", "POST"])
+def logistic():
+    model_score, params_table, result_table, class_report, conf_table = wine_type()
+    return redirect(url_for("index.html"), code=302)
+    
 if __name__ == "__main__":
     app.run(debug=True)
 
